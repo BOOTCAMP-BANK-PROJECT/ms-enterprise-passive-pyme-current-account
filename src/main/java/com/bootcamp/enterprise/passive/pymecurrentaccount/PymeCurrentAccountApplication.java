233ab3773cc641648fcb102fe8e15b9120317e70
samuel14luis <luismendoza.samuel@gmail.com>
2022-04-20 14:14:48 -0500

    scaffolding

---
+++ src/main/java/com/bootcamp/enterprise/passive/pymecurrentaccount/PymeCurrentAccountApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.enterprise.passive.pymecurrentaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PymeCurrentAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PymeCurrentAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/enterprise/passive/pymecurrentaccount/PymeCurrentAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.enterprise.passive.pymecurrentaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PymeCurrentAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
